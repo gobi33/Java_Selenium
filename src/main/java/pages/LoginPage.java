a8e84bb2d7cbe62e87f1d6b0f21ce502e2741d2d
gobi33 <164046407+gobi33@users.noreply.github.com>
2026-08-25 14:46:20 +0530

    Clean up LoginPage.java by removing commented code
    Removed commented-out code for the LoginPage class.

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,39 +1,4 @@
-// package pages;
-
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-
-// public class LoginPage {
-
-//     WebDriver driver;
-
-//     public LoginPage(WebDriver driver) {
-//         this.driver = driver;
-//     }
-
-//     // Locators
-//     By loginLink = By.className("ico-login");
-//     By email = By.id("Email");
-//     By password = By.id("Password");
-//     By loginButton = By.xpath("//input[@class='button-1 login-button']");
-
-//     // Actions
-//     public void clickLoginLink() {
-//         driver.findElement(loginLink).click();
-//     }
-
-//     public void enterEmail(String mail) {
-//         driver.findElement(email).sendKeys(mail);
-//     }
-
-//     public void enterPassword(String pwd) {
-//         driver.findElement(password).sendKeys(pwd);
-//     }
-
-//     public void clickLoginButton() {
-//         driver.findElement(loginButton).click();
-//     }
-// }
+//gobinath changes 
 
 package pages;
 
